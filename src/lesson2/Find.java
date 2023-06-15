4d4847dd871537d7b76ba5166c41cfe67ff64a29
andrey <naumov.a.n@icloud.com>
2023-06-16 00:08:52 +0700

    seminar2

--- src/lesson2/Find.java
+++ src/lesson2/Find.java
@@ -12,14 +12,15 @@ public class Find {
         Sort.insertSort(arr);
         System.out.println(Arrays.toString(arr));
         //int num = find(arr, 15);    // O(n)
-        int num = binarySearch(arr, 40, 0, arr.length - 1) ;    // O(log n)
+        //int num = binarySearch(arr, 40, 0, arr.length - 1);    // O(log n)
+        int num = binarySearchW(arr, 44);
         System.out.println(num);
     }
 
     // Поиск в массиве
-    public static int find(int[] array, int value){
+    public static int find(int[] array, int value) {
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == value){
+            if (array[i] == value) {
                 return i;
             }
         }
@@ -27,24 +28,38 @@ public class Find {
     }
 
     // O(log n) Бинарный поиск
-    public static int binarySearch(int[] array, int value, int min_value, int max_value){
+    public static int binarySearch(int[] array, int value, int min_value, int max_value) {
         int midpoint;
-        if (max_value < min_value){
+        if (max_value < min_value) {
             return -1;
-        }else {
+        } else {
             midpoint = (max_value - min_value) / 2 + min_value;
         }
 
-        if (array[midpoint] < value){
+        if (array[midpoint] < value) {
             return binarySearch(array, value, midpoint + 1, max_value);
         } else {
-            if (array[midpoint] > value){
-                return binarySearch(array, value, min_value, midpoint-1);
-            }else {
+            if (array[midpoint] > value) {
+                return binarySearch(array, value, min_value, midpoint - 1);
+            } else {
                 return midpoint;
             }
         }
     }
 
-    //44:00 lesson2 56
+    // Бинарный поиск (через цикл)
+    public static Integer binarySearchW(int[] array, int value) {
+        int left = 0, right = array.length - 1;
+        while (right - left > 1) {
+            int mid = (left + right) / 2;
+            if (array[mid] > value) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        if (array[left] == value) return left;
+        if (array[right] == value) return right;
+        return null;
+    }
 }
--- src/lesson2/Sort.java
+++ src/lesson2/Sort.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Sort {
+    final static int[] buf = new int[100000];
+
     public static void main(String[] args) {
         int[] arr = new int[]{
                 4, 6, 5, 15, 8, 2, 44, 6, 4, 1, 7, 25
@@ -12,11 +14,13 @@ public class Sort {
         //bubbleSort(arr);  // O(n^2)
         //directSort(arr);    // O(n^2)
         //insertSort(arr);    // O(n^2)
-        ss(arr);
+        //shakeSort(arr);     // O(n^2)
+        mergeSort(arr);
         System.out.println(Arrays.toString(arr));
     }
+
     // O(n^2) Пузырьковая сортировка
-    public static void bubbleSort(int[] arr){
+    public static void bubbleSort(int[] arr) {
         boolean finish;
         //int cnt = 0;
         do {
@@ -30,27 +34,43 @@ public class Sort {
                     //cnt++;
                 }
             }
-        }while (!finish);
+        } while (!finish);
         //System.out.println(cnt);
     }
 
-    // Шейкерная сортировка
-    public static void ss(int[] array){
+    // // O(n^2) Шейкерная сортировка
+    public static void shakeSort(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            for (int j = i; j < array.length - i - 1; i++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
 
+            for (int j = array.length - i - 1; j > i; i--) {
+                if (array[j - 1] > array[j]) {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
     }
 
     // O(n^2) Сортировка выбором
-    public static void directSort(int[]array){
+    public static void directSort(int[] array) {
         //int cut = 0;
-        for (int i = 0; i < array.length - 1; i++){
+        for (int i = 0; i < array.length - 1; i++) {
             int minPosition = i;
-            for (int j = i + 1; j < array.length; j++){
-                if (array[j] < array[minPosition]){
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minPosition]) {
                     minPosition = j;
                     //cut++;
                 }
             }
-            if (i != minPosition){
+            if (i != minPosition) {
                 int temp = array[i];
                 array[i] = array[minPosition];
                 array[minPosition] = temp;
@@ -60,20 +80,59 @@ public class Sort {
     }
 
     // // O(n^2) Сортироака вставкой
-    public static void insertSort(int[] array){
+    public static void insertSort(int[] array) {
         //int cnt = 0;
-        for (int i = 0; i < array.length - 1; i++){
-            for (int j = i + 1; j < array.length; j++){
-                if (array[i] > array[j]){
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
                     int temp = array[i];
                     array[i] = array[j];
                     array[j] = temp;
-                   // cnt++;
+                    // cnt++;
                 }
             }
         }
         //System.out.println(cnt);
     }
 
+    // Сортировка слиянием
+    static public void mergeSort(int[] Array) {
+        mergeSort(Array, 0, Array.length - 1);
+    }
+
+    static private void mergeSort(int[] Array, int left, int right) {
+        if (left == right) {
+            return;
+        }
+        int mid = (left + right) / 2;
+        mergeSort(Array, left, mid);
+        mergeSort(Array, mid + 1, right);
+
+        //Слияние
+        int i = left, j = mid + 1, k = left;
+        while (i <= mid && j <= right) {
+            if (Array[i] < Array[j]) {
+                buf[k] = Array[i];
+                i++;
+            } else {
+                buf[k] = Array[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= mid) {
+            buf[k++] = Array[i++];
+        }
+
+        while (j <= right) {
+            buf[k++] = Array[j++];
+        }
+
+        for (int q = left; q <= right; q++)
+            Array[q] = buf[q];
+    }
 
 }
+
+
